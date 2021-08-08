d01266a73cd0c7018b3a3847938a51dc95985710
Nicolas Andrew Burnett <nicolasandrewburnett@gmail.com>
2021-08-07 19:31:34 -0500

    Transitioned to tailwind on program pages

--- components/ProgramPage.js
+++ components/ProgramPage.js
@@ -13,34 +13,39 @@ export class Testimonial extends Component {
   constructor(props) {
     super(props);
     this.state = {};
+    this.mainStyle = 'flex flex-wrap items-top mb-5 text-base min-w-4/5 max-w-4/5 lg:flex-nowrap'
+    this.containerStyle = 'flex flex-col max-w-3/5'
+    this.titleStyle = 'text-base font-black'
+    this.descriptionStyle = 'text-sm'
+    this.imageStyle = 'rounded-full w-1/5 m-5 lg:w-1/3'
   }
   render() {
     if (this.props.right) {
       return (
         <div
-          className={styles.testimonial}
+          className={this.mainStyle}
           style={{ alignSelf: 'flex-end', justifyContent: 'flex-end' }}
         >
-          <div className={styles.testimonial_container}>
-            <div className={styles.testimonial_name}>{this.props.name}</div>
-            <div className={styles.testimonial_description}>
+          <div className={this.containerStyle}>
+            <div className={this.titleStyle}>{this.props.name}</div>
+            <div className={this.descriptionStyle}>
               {this.props.description}
             </div>
           </div>
-          <div className={styles.testimonial_image}>
-            <img src={this.props.image} />
+          <div className={this.imageStyle}>
+            <img className='rounded-full' style={{ aspectRatio: 1 }} src={this.props.image} />
           </div>
         </div>
       );
     }
     return (
-      <div className={styles.testimonial}>
-        <div className={styles.testimonial_image}>
-          <img src={this.props.image} />
+      <div className={this.mainStyle}>
+        <div className={this.imageStyle}>
+          <img className='rounded-full' style={{ aspectRatio: 1 }} src={this.props.image} />
         </div>
-        <div className={styles.testimonial_container}>
-          <div className={styles.testimonial_name}>{this.props.name}</div>
-          <div className={styles.testimonial_description}>
+        <div className={this.containerStyle}>
+          <div className={this.titleStyle}>{this.props.name}</div>
+          <div className={this.descriptionStyle}>
             {this.props.description}
           </div>
         </div>
@@ -63,6 +68,7 @@ export default class ProgramPage extends Component {
   constructor(props) {
     super(props);
     this.state = {};
+    this.infoStyle = 'flex items-center justify-center my-10'
   }
   render() {
     const testimonials = [];
@@ -81,47 +87,42 @@ export default class ProgramPage extends Component {
     });
     return (
       <div style={{ '--accent': this.props.accent || '#ff00ff' }}>
-        <div className={styles.logo}>{this.props.program}</div>
-        <div className={styles.info_container}>
-          <Info id={styles.team_info} title="Why us?" body={this.props.why} />
+        <div className='flex items-center justify-center mb-14'>{this.props.program}</div>
+        <div className={this.infoStyle}>
+          <Info title="Why us?" body={this.props.why} />
         </div>
-        <div className={styles.info_container}>
+        <div className={this.infoStyle}>
           <Info
-            id={styles.team_info}
             title="Program Benefits"
             body={this.props.benefits}
           />
         </div>
-        <ImageCarousel id={styles.images} images={this.props.images} />
-        <div className={styles.info_container}>
+        <ImageCarousel images={this.props.images} />
+        <div className={this.infoStyle}>
           <Info
-            id={styles.team_info}
             title="How do I join?"
             body={this.props.how}
           />
         </div>
-        <div className={styles.info_container}>
+        <div className={this.infoStyle}>
           <div
             onClick={() => window.open(this.props.link)}
-            className={styles.join}
-            style={{ '--left': this.props.left, '--right': this.props.right }}
+            className='join transition cursor-pointer rounded-2xl p-2 px-12 mx-24 my-3 hover:filter'
+            style={{ '--left': this.props.left, '--right': this.props.right, '--tw-brightness': 'brightness(140%)', }}
           >
             Apply now!
           </div>
         </div>
         {this.props.projects && this.props.projects.length > 0 ? (
-          <div className={styles.projects_container}>
-            <div className={styles.projects_title}>Past Submissions</div>
-            <div className={styles.projects}>{this.props.projects}</div>
+          <div className='flex items-center justify-center flex-wrap text-sm mb-14'>
+            <div className='m-0 text-xl font-bold text-center'>Past Submissions</div>
+            <div className='flex flex-wrap items-center justify-center text-base'>{this.props.projects}</div>
           </div>
         ) : (
           <div />
         )}
         {testimonials.length > 0 ? (
-          <div className={styles.testimonials_container}>
-            <div className={styles.testimonials_title}>Testimonials</div>
-            <div className={styles.testimonials}>{testimonials}</div>
-          </div>
+          <div className='m-auto flex flex-col w-4/5'>{testimonials}</div>
         ) : (
           <div />
         )}
