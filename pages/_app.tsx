0b372df562174c377395ed97f038798b4031afc3
Willie Chalmers III <willie.chalmers@acmutd.co>
2021-08-11 17:34:15 -0700

    Refactor _app.tsx to use TypeScript

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import Navigation from '../components/Navigation';
 import Footer from '../components/Footer';
 import '../styles/globals.css';
+import { AppProps } from 'next/dist/next-server/lib/router/router';
 
-function Website({ Component, pageProps }) {
+/**
+ * A wrapper for the website.
+ *
+ * This displays a common header across all pages.
+ */
+function Website({ Component, pageProps }: AppProps) {
   return (
     <div id="app" className="relative">
       <Navigation {...pageProps} />
